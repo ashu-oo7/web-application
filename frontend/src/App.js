35e0db35302f1898d5dccac7124209377f450bf8
ashu-oo7 <ashu0705000@gmail.com>
2024-08-07 20:27:24 +0530

    creating pages for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import { Routes,Route } from 'react-router-dom';
+import Homepage from "./Pages/Homepage";
+import Chatpage from "./Pages/ChatPage"
 function App() {
   return (
-    <div>Running the App</div>
-    );
+    <div className="App">
+      <Routes>
+
+        <Route path="/" Component={Homepage} exact />
+        <Route path="/chats" Component={Chatpage} />
+
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
